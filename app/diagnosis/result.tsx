236ae033a00168eb8e593e737e280ddb0e1916c3
Rsprt <l3k1yg@gmail.com>
2025-04-04 03:29:23 +0900

    화면 수정

--- app/diagnosis/result.tsx
+++ app/diagnosis/result.tsx
@@ -1,26 +1,62 @@
 import { View, Text, StyleSheet, Image } from 'react-native';
 import { useLocalSearchParams } from 'expo-router';
 
-export default function ResultScreen() {
+export default function DiagnosisResultScreen() {
   const { image, description } = useLocalSearchParams();
+  const imageUri = Array.isArray(image) ? image[0] : image;
+  const descText = Array.isArray(description) ? description[0] : description;
 
   return (
     <View style={styles.container}>
+
       <Text style={styles.title}>진단 결과</Text>
 
-      {image && (<Image source={{ uri: image as string }} style={styles.image} />)}
+      {imageUri ? (
+        <Image
+          source={{ uri: imageUri }}
+          style={styles.image}
+          resizeMode="cover"
+          //onError={(e) => console.log('이미지 로드 실패:', e.nativeEvent.error)}
+        />
+      ) : (
+        <Text>이미지가 없습니다.</Text>
+        //http이미지 출력은 가능
+      )} 
+
       <Text style={styles.label}>설명:</Text>
-      <Text>{description}</Text> {/* 임시 */}
+      <Text>{descText}</Text>
       <Text style={styles.label}>이미지 URI:</Text>
-      <Text>{image}</Text> {/* 임시 */}
+      <Text>{imageUri}</Text>
+
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
-    container: { flex: 1, padding: 20, backgroundColor: '#fff' },
-    title: { fontSize: 22, fontWeight: 'bold', marginBottom: 20 },
-    image: { width: '100%', height: 200, borderRadius: 10, marginBottom: 20 },
-    label: { fontWeight: 'bold', marginTop: 10 },
-    text: { fontSize: 16, marginTop: 5 },
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#fff'
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginBottom: 20
+  },
+  image: {
+    width: '100%',
+    height: 200,
+    borderRadius: 10,
+    marginBottom: 20,
+    backgroundColor: '#eee',
+  },
+  label: {
+    fontWeight: 'bold',
+    marginTop: 10
+  },
+  text: {
+    fontSize: 16,
+    marginTop: 5
+  },
 });
