2d41875bb10dcddc205002a07f79059fbd4f9de6
Rsprt <l3k1yg@gmail.com>
2025-05-10 17:14:59 +0900

    진단 디자인추가

--- app/(tabs)/diagnosis.tsx
+++ app/(tabs)/diagnosis.tsx
@@ -6,7 +6,8 @@ import * as SecureStore from 'expo-secure-store';
 export default function DiagnosisScreen() {
   const [plants, setPlants] = useState<Plant[]>([]);
   const router = useRouter();
-  
+  const allowedPlants = ['딸기', '토마토', '포도', '호박', '오이', '상추', '고추', '감자', '파프리카']; // 진단 가능 식물 목록 
+
   type Plant = { // 필요한 식물 정보 타입
     id: number; // 식물 id
     name: string; // 식물 이름
@@ -21,13 +22,12 @@ export default function DiagnosisScreen() {
   const fetchMyPlants = async () => {
     try {
       const token = await SecureStore.getItemAsync('userToken');
-
-      const response = await fetch('http://192.108.0.X:8080', { // 백엔드 ip주소
-      method: 'GET',
-      headers: {
-        Authorization: `Bearer ${token}`, // JWT 토큰 
-      },
-    });
+      const response = await fetch('http://43.202.4.163:8080/api/disease/predict', { // 백엔드 ip주소
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${token}`, // JWT 토큰 
+        },
+      });
 
       const data = await response.json();
       setPlants(data); // 사용자의 식물 정보 받아오기
@@ -39,41 +39,59 @@ export default function DiagnosisScreen() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>진단이 필요한 식물을 선택해주세요</Text>
+      <Text style={styles.title}>진단이 필요한 친구를 선택해주세요</Text>
 
-      <ScrollView contentContainerStyle={{ gap: 12 }}>
+      <ScrollView contentContainerStyle={{ gap: 16, paddingBottom: 50 }} showsVerticalScrollIndicator={false}>
         {plants.length === 0 ? (
-          <Text style={styles.card}>내 식물이 없습니다.</Text>
+          <Text style={styles.emptyMessage}>내 식물이 없습니다.</Text>
         ) : (
-          plants.map((plant) => (
-            <TouchableOpacity
-              key={plant.id}
-              style={styles.card}
-              onPress={() =>
-                router.push({
-                  pathname: '/diagnosis/select',
-                  params: { name: plant.name },
-                })
-              }
-            >
-              <View style={styles.imageBox}>
-                {plant.image ? (
-                  <Image source={{ uri: plant.image }} style={styles.image} />
-                ) : (
-                  <Text style={styles.imageText}>사진</Text>
-                )}
+          plants.map((plant) => {
+            const isAllowed = allowedPlants.includes(plant.name);
+            return (
+              <View key={plant.id} style={styles.card}>
+                <View style={styles.imageBox}>
+                  {plant.image ? (
+                    <Image source={{ uri: plant.image }} style={styles.image} resizeMode="cover" />
+                  ) : (
+                    <Text style={styles.imageText}>사진 없음</Text>
+                  )}
+                </View>
+                <View style={styles.cardTextBox}>
+                  <Text style={styles.plantName}>이름: {plant.name}</Text>
+                  <Text style={styles.plantStatus}>상태: {plant.status}</Text>
+                  <TouchableOpacity
+                    style={isAllowed ? styles.myPlantSelectButton : styles.myPlantSelectButton2}
+                    activeOpacity={0.85}
+                    onPress={() => {
+                      if (isAllowed) {
+                        router.push({
+                          pathname: '/diagnosis/select',
+                          params: { name: plant.name },
+                        });
+                      }
+                    }}
+                    disabled={!isAllowed}
+                  >
+                    <Text style={styles.myPlantSelectButtonText}>
+                      {isAllowed ? '선택하기' : '진단 불가'}
+                    </Text>
+                  </TouchableOpacity>
+                </View>
               </View>
-              <View>
-                <Text>이름 : {plant.name}</Text>
-                <Text>상태 : {plant.status}</Text>
-              </View>
-            </TouchableOpacity>
-          ))
+            );
+          })
         )}
         <TouchableOpacity
           style={styles.selectButton}
+          activeOpacity={0.85}
           onPress={() => router.push('/diagnosis/select')}>
           <Text style={styles.selectButtonText}>사진으로 진단</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.historyButton}
+          activeOpacity={0.85}
+          onPress={() => router.push('/diagnosis/history')}>
+          <Text style={styles.historyButtonText}>진단기록 확인</Text>
         </TouchableOpacity>
       </ScrollView>
     </View>
@@ -81,55 +99,111 @@ export default function DiagnosisScreen() {
 }
 
 const styles = StyleSheet.create({
-  container: { 
-    flex: 1, 
-    padding: 20, 
-    backgroundColor: '#fff' 
+  container: {
+    flex: 1,
+    padding: 18,
+    backgroundColor: '#F0F0F0',
   },
-  title: { 
-    fontSize: 18, 
-    fontWeight: 'bold', 
-    marginBottom: 20 
+  title: {
+    fontSize: 20,
+    marginBottom: 30,
+    color: '#363636',
+    textAlign: 'center',
   },
   card: {
     flexDirection: 'row',
     alignItems: 'center',
-    backgroundColor: '#eee',
-    padding: 12,
-    borderRadius: 10,
-    gap: 16,
+    backgroundColor: '#FFFFFF',
+    borderRadius: 30,
+    marginBottom: 8,
   },
   imageBox: {
-    width: 60,
-    height: 60,
-    backgroundColor: '#ccc',
+    width: 180,
+    height: 180,
+    backgroundColor: '#D4EAE1',
     justifyContent: 'center',
     alignItems: 'center',
-    borderRadius: 8,
+    borderRadius: 30,
+    overflow: 'hidden',
   },
   image: {
-    width: 60,
-    height: 60,
-    borderRadius: 8,
+    width: '100%',
+    height: '100%',
+    borderRadius: 14,
+    objectFit: 'cover',
   },
-  imageText: { 
-    color: '#666' 
+  imageText: {
+    color: '#9E9E9E',
+    fontSize: 14,
+  },
+  cardTextBox: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  plantName: {
+    fontSize: 12,
+    fontWeight: 'bold',
+    color: '#363636',
+    marginTop: 30,
+    marginBottom: 10,
+    textAlign: "center",
+  },
+  plantStatus: {
+    fontSize: 10,
+    color: '#9E9E9E',
+    marginBottom: 30,
+    textAlign: "center",
+  },
+  myPlantSelectButton: {
+    backgroundColor: '#00D282',
+    paddingVertical: 8,
+    paddingHorizontal: 24,
+    borderRadius: 20,
+    alignSelf: 'center',
+    textAlign: "center",
+  },
+  myPlantSelectButton2: {
+    backgroundColor: '#9E9E9E',
+    paddingVertical: 8,
+    paddingHorizontal: 24,
+    borderRadius: 20,
+    alignSelf: 'center',
+    textAlign: "center",
+  },
+  myPlantSelectButtonText: {
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+    fontSize: 12,
   },
   selectButton: {
-    marginTop: 20,
-    backgroundColor: '#ccc',
-    padding: 16,
+    marginTop: 10,
+    backgroundColor: '#00D282',
+    padding: 18,
     alignItems: 'center',
-    borderRadius: 10,
+    borderRadius: 16,
   },
   selectButtonText: {
     fontSize: 16,
     fontWeight: 'bold',
+    color: '#FFFFFF',
+  },
+  historyButton: {
+    marginTop: 5,
+    marginBottom: 30,
+    backgroundColor: '#FDDB83',
+    padding: 18,
+    alignItems: 'center',
+    borderRadius: 16,
+  },
+  historyButtonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#FFFFFF',
   },
   emptyMessage: {
     textAlign: 'center',
     marginTop: 40,
     fontSize: 16,
-    color: '#888',
+    color: '#9E9E9E',
   },
 });
--- app/diagnosis/result.tsx
+++ app/diagnosis/result.tsx
@@ -1,77 +1,237 @@
-import { View, Text, StyleSheet, Image } from 'react-native';
-import { useLocalSearchParams } from 'expo-router';
+import { ScrollView, View, Text, StyleSheet, Image } from 'react-native';
+import { useLocalSearchParams, useRouter } from 'expo-router';
+import { TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons'
+import { AnimatedCircularProgress } from 'react-native-circular-progress';
 
 export default function DiagnosisResultScreen() {
   const { image, result, confidence } = useLocalSearchParams();
+  const router = useRouter();
+
   const imageUri = Array.isArray(image) ? image[0] : image; // 이미지 
   const predictResult = Array.isArray(result) ? result[0] : result; // 병명 
-  const predictConfidence = Array.isArray(confidence) ? confidence[0] : confidence; // 정확도 
+  const predictConfidence = parseFloat(
+    Array.isArray(confidence) ? confidence[0] : confidence
+  ); // 정확도 
+
+  const percentage = isNaN(predictConfidence) ? 0 : Math.round(predictConfidence * 100);
+
+
+  const dummytreatments = [
+    {
+      id: '1',
+      icon: require('../../assets/images/book_icon.png'),
+      title: '수분 공급',
+      description: '매일 아침, 물을 주세요.',
+    },
+    {
+      id: '2',
+      icon: require('../../assets/images/book_icon.png'),
+      title: '자외선 조절',
+      description: '매일 오후 2시에 그늘에 놓으세요.',
+    },
+    {
+      id: '3',
+      icon: require('../../assets/images/book_icon.png'),
+      title: '비료 공급',
+      description: '2주에 한 번 비료를 주세요.',
+    },
+  ]; // 더미 데이터
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>진단 결과</Text>
-
-      {imageUri ? (
-        <Image
-          source={{ uri: imageUri }}
-          style={styles.image}
-          resizeMode="cover"
-          onError={(e) => console.log('이미지 로드 실패:', e.nativeEvent.error)}
-        />
-      ) : (
-        <Text>이미지가 없습니다.</Text>
-      )}
-
-      <View style={styles.detail}>
-        <Text style={styles.resultLabel}>예측된 질병</Text>
-        <Text style={styles.resultValue}>{predictResult || '없음'}</Text>
-
-        <Text style={styles.resultLabel}>정확도</Text>
-        <Text style={styles.resultValue}>{predictConfidence ? `${(parseFloat(predictConfidence) * 100).toFixed(1)}%` : 'N/A'}</Text>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={() => router.replace('/diagnosis')} style={styles.backButton}>
+          <Ionicons name="arrow-back" size={24} color="#FFFFFF" />
+        </TouchableOpacity>
+        <Text style={styles.headerTitle}>식물 진단 결과</Text>
       </View>
-    </View>
 
+      <ScrollView contentContainerStyle={styles.scrollContent}>
+        <View style={styles.section}>
+          <View style={styles.sectionTitleContainer}>
+            <Image source={require('../../assets/images/plant_icon.png')} style={styles.icon} />
+            <Text style={styles.sectionTitle}>진단 결과</Text>
+          </View>
+
+          <View style={styles.gaugeContainer}>
+            <AnimatedCircularProgress // 반달 게이지
+              size={180}
+              width={15}
+              fill={percentage}
+              tintColor="#00D282"
+              backgroundColor="#EEEEEE"
+              rotation={-90} // 90도 회전
+              arcSweepAngle={180}
+              lineCap="round"
+            >
+              {() => (
+                <View style={styles.labelContainer}>
+                  <Text style={styles.statusText}>정확도</Text>
+                  <Text style={styles.percentText}>{percentage}%</Text>
+                </View>
+              )}
+            </AnimatedCircularProgress>
+            <Text style={styles.labelText}>진단명</Text>
+            <Text style={styles.diagnosisText}>{predictResult || '진단명 없음'}</Text>
+          </View>
+
+          <View style={styles.imageBox}>
+            {imageUri ? (
+              <Image
+                source={{ uri: imageUri }}
+                style={styles.image}
+                resizeMode="cover"
+                onError={(e) => console.log('이미지 로드 실패:', e.nativeEvent.error)} // 이미지 출력 확인
+              />
+            ) : (
+              <Text>이미지가 없습니다.</Text>
+            )}
+          </View>
+        </View>
+
+        {/* 치료 방법 */}
+        <View style={styles.section}>
+          <View style={styles.sectionTitleContainer}>
+            <Image source={require('../../assets/images/plant_icon.png')} style={styles.icon} />
+            <Text style={styles.sectionTitle}>진단과 관련된 치료방법</Text>
+          </View>
+          {dummytreatments.map((item) => (
+            <View key={item.id} style={styles.treatmentItem}>
+              <Image source={item.icon} style={styles.treatmentIcon} />
+              <View style={styles.treatmentText}>
+                <Text style={styles.treatmentTitle}>{item.title}</Text>
+                <Text style={styles.treatmentDesc}>{item.description}</Text>
+              </View>
+            </View>
+          ))}
+        </View>
+      </ScrollView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
-    backgroundColor: '#fff',
+    backgroundColor: '#F8F8F8',
+  },
+  header: {
+    backgroundColor: '#00D282',
+    paddingTop: 20,
+    paddingBottom: 20,
     alignItems: 'center',
   },
-  title: {
+  headerTitle: {
+    color: '#FFFFFF',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  scrollContent: {
+    paddingBottom: 20,
+  },
+  section: {
+    marginTop: 20,
+    paddingHorizontal: 20,
+  },
+  sectionTitleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  icon: {
+    width: 20,
+    height: 20,
+    marginRight: 6,
+    resizeMode: 'contain',
+  },
+  sectionTitle: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#363636',
+  },
+  sectionBox: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF',
+    borderRadius: 30,
+    marginBottom: 8,
+  },
+  gaugeContainer: {
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  labelContainer: {
+    position: 'absolute',
+    top: 20,
+    alignItems: 'center',
+    width: 100,
+  },
+  statusText: {
+    fontSize: 12,
+    color: '#9E9E9E',
+  },
+  percentText: {
     fontSize: 24,
     fontWeight: 'bold',
+    color: '#00D282',
+  },
+  labelText: {
+    fontSize: 14,
+    marginTop: -70,
+    color: '#363636',
+    fontWeight: 'bold',
+  },
+  diagnosisText: {
+    fontSize: 16,
+    color: '#363636',
+    marginTop: 10,
+  },
+  imageBox: {
+    alignItems: 'center',
     marginBottom: 20,
   },
   image: {
-    width: '100%',
-    height: 220,
+    width: 300,
+    height: 300,
     borderRadius: 12,
-    marginBottom: 20,
-    backgroundColor: '#eee',
+    backgroundColor: '#EEEEEE',
   },
-  detail: {
-    backgroundColor: '#fff',
-    padding: 20,
+  treatmentItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  treatmentIcon: {
+    width: 32,
+    height: 32,
+    marginRight: 12,
+    resizeMode: 'contain',
     borderRadius: 16,
-    width: '100%',
+    backgroundColor: '#F0F0F0',
   },
-  resultLabel: {
+  treatmentText: {
+    flex: 1,
+  },
+  treatmentTitle: {
     fontSize: 14,
     fontWeight: 'bold',
-    color: '#555',
-    marginTop: 12,
+    color: '#363636',
   },
-  resultValue: {
-    fontSize: 18,
-    color: '#222',
-    marginTop: 4,
+  treatmentDesc: {
+    fontSize: 12,
+    color: '#9E9E9E',
+    marginTop: 2,
   },
-  errorText: {
-    color: 'red',
-    marginBottom: 20,
+  backButton: {
+    position: 'absolute',
+    left: 10,
+    top: 15,
+    padding: 8,
+  },
+  diseaseButton: {
+    color: '#FFFFFF',
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
